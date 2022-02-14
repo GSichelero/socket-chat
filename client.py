1c6a789a7c288498afa5155205173dc62a01a0e3
Gabriel Sichelero <gabriel.sichelero@hotmail.com>
2022-02-14 16:50:59 -0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,32 @@
+import datetime
+import select
+import socket
+import sys
+
+PORT = 5050
+SERVER = "192.168.0.105"
+ADDR = (SERVER, PORT)
+
+server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_connection.connect(ADDR)
+
+user_name = input("Qual o seu nome? ")
+
+while True:
+    sockets_list = [sys.stdin, server_connection]
+    read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+ 
+    for connection_received in read_sockets:
+        if connection_received == server_connection:
+            message = connection_received.recv(4096).decode('utf-8')
+            now = datetime.datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print(f'{current_time} {message}', flush = True)
+        else:
+            message = input()
+            sys.stdout.write("\033[F")
+            now = datetime.datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print(f'{current_time} Você: {message}')
+            server_message_full_information = f'{user_name}::{current_time}::{message}'
+            server_connection.send(server_message_full_information.encode('utf-8'))
---
+++ server.py
@@ -0,0 +1,69 @@
+import threading
+import datetime
+import socket
+
+PORT = 5050
+SERVER = "192.168.0.105"
+ADDR = (SERVER, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(ADDR)
+server.listen(10)
+ 
+clients_connected = []
+
+def cancel_clients_connection(connection):
+    if connection in clients_connected:
+        clients_connected.remove(connection)
+
+
+def send_message_to_all_other_clients(message, connection):
+    for clients in clients_connected:
+        if clients!=connection:
+            try:
+                clients.send(message.encode('utf-8'))
+            except:
+                clients.close()
+                cancel_clients_connection(clients)
+
+
+def handle_client(conn, addr):
+    print(f"Usuário {addr} conectou!", flush = True)
+    conn.send("Bem-vindo a sala de chat!".encode('utf-8'))
+    connected = True
+    while connected:
+        message = conn.recv(4096).decode("utf-8")
+        if not message:
+            now = datetime.datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            connected = False
+            cancel_clients_connection(conn)
+            final_message = f'{addr} foi desconectado em {current_time}!'
+        elif message.split("::")[2] == "!Desconectar":
+            user_name = message.split("::")[0]
+            message_sent_time = message.split("::")[1]
+            connected = False
+            cancel_clients_connection(conn)
+            final_message = f'Usuário {user_name} se desconectou em {message_sent_time}!'
+        else:
+            user_name = message.split("::")[0]
+            message_sent_time = message.split("::")[1]
+            message_text = message.split("::")[2]
+            final_message = f'{user_name}: {message_text}\nEnviada as: {message_sent_time}'
+        ip_address = str(addr).split("'")[1]
+        port_address = str(str(addr).split(", ")[1])[:-1]
+        print(f'IP:{ip_address} Port:{port_address} Username:{final_message}', flush = True)
+        send_message_to_all_other_clients(final_message, conn)
+
+
+def start():
+    print('Server Iniciado!', flush=True)
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        clients_connected.append(conn)
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+
+start()
